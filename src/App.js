0567816134323bd5511dbc77f6817f7ae76ada97
jeevaf22 <48169919+Jeeva-ss@users.noreply.github.com>
2022-11-07 13:42:21 +0530

    CRA & Tailwind CSS added 🚀

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae neque impedit vel sunt voluptatum! Numquam amet tempora, esse iste temporibus, blanditiis exercitationem iure soluta voluptatibus aliquid dolore? Magnam, sequi laborum?</p>
+    </div>
+  );
+}
+
+export default App;
