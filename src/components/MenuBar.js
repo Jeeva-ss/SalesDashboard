e49eb6b67f03fca21548a927931f94bafad3246f
jeevaf22 <48169919+Jeeva-ss@users.noreply.github.com>
2022-11-07 23:44:38 +0530

    Update MenuBar.js

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -5,27 +5,27 @@ export default function MenuBar() {
       <div className="flex items-center justify-between gap-1">
         <div className="flex flex-col items-center gap-1">
           <i className="text-white fa-solid fa-table-columns"></i>
-          <a className="text-xs" href="#">Home</a>
+          <a className="text-xs" href="/">Home</a>
         </div>
         <div className="flex flex-col items-center gap-1">
           <i className="text-white fa-solid fa-diagram-project"></i>
-          <a className="text-xs" href="#">Projects</a>
+          <a className="text-xs" href="/">Projects</a>
         </div>
         <div className="flex flex-col items-center gap-1">
           <i className="text-white fa-solid fa-list-check "></i>
-          <a className="text-xs" href="#">Billing</a>
+          <a className="text-xs" href="/">Billing</a>
         </div>
         <div className="flex flex-col items-center gap-1">
           <i className="text-white fa-solid fa-calendar "></i>
-          <a className="text-xs" href="#">Members</a>
+          <a className="text-xs" href="/">Members</a>
         </div>
         <div className="flex flex-col items-center gap-1">
           <i className="text-white fa-solid fa-chart-simple "></i>
-          <a className="text-xs" href="#">Stats</a>
+          <a className="text-xs" href="/">Stats</a>
         </div>
         <div className="flex flex-col items-center gap-1">
           <i className="text-white fa-solid fa-gear "></i>
-          <a className="text-xs" href="#">Settings</a>
+          <a className="text-xs" href="/">Settings</a>
         </div>
       </div>
     </div>
